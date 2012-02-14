20e1fca53ce25365a88974cf7c610c0759ccea19
Kaique Silva <kaique.developer@gmail.com>
2012-02-14 13:43:15 -0200

    mini site ok revision cruching

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// Default script
+
+var showItem = function ( element, duration ) {
+	$(element).fadeTo(0, 0);
+
+	setTimeout( function () {
+		$( element ).fadeTo( duration, 1);
+	}, duration);
+};
+
+$(function () {
+	var element = {
+		title: '.main-content article header div h2',
+		sep: '.main-content article header div hr',
+		menu: '.inner-left-menu nav',
+		innerRightContent: '.inner-right-content',
+		bubble: 'div.bubble',
+		footer: '.main-content article footer'
+	};
+	
+	// Show Title
+	showItem( element.title, 1000 );
+	// Show Title
+	showItem( element.sep, 1000 );
+	// Show menu
+	showItem( element.menu, 1500 );
+	// Show box of content
+	showItem( element.innerRightContent, 1800 );
+	// Show bubble
+	showItem( element.bubble, 2800 );
+	// Show footer
+	showItem( element.footer, 2200 );
+});
